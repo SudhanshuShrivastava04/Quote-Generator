db058fbe09d8458bcae6d256bd2dc570e4720b71
SudhanshuShrivastava04 <211230053@nitdelhi.ac.in>
2023-07-31 23:19:29 +0530

    more quotes added

--- src/App.js
+++ src/App.js
@@ -32,6 +32,15 @@ function App() {
     "Life is what happens when you're busy making other plans.",
     "The future belongs to those who believe in the beauty of their dreams.",
     "The only limit to our realization of tomorrow will be our doubts of today.",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    "The only way to do great work is to love what you do.",
+    "Believe you can and you're halfway there.",
+    "The only person you are destined to become is the person you decide to be.",
+    "Don't watch the clock; do what it does. Keep going.",
+    "It does not matter how slowly you go as long as you do not stop.",
+    "In the end, we will remember not the words of our enemies, but the silence of our friends.",
+    "Success is walking from failure to failure with no loss of enthusiasm.",
+    "The best way to predict the future is to create it."
   ];
   const [bgColorIndex, setBgColorIndex] = useState(0);
   const [quote, setQuote] = useState(0);
